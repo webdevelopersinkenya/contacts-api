47851a10790db653b40c687f57b089762450845b
mir22006@byui.edu <mirah.gabriel1969@gmail.com>
2026-05-13 18:03:19 +0300

    changes

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const connectDB = require('./db/connect');
-
-//  FIRST
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/contacts', require('./routes/contacts'));
-
-// Swagger
+// Swagger FIRST (important)
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// THEN routes
+app.use('/contacts', require('./routes/contacts'));
+
+const connectDB = require('./db/connect');
 
 const PORT = process.env.PORT || 8080;
 
