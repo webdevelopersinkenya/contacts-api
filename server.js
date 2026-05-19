16622e71ea2f276eddf6ec5cad6090dd30aa4657
mir22006@byui.edu <mirah.gabriel1969@gmail.com>
2026-05-19 14:25:52 +0300

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const userRoutes = require("./routes/users");
+const userRoutes = require("./routes/Users");
 const errorHandler = require("./middleware/errorHandler");
 
 app.use(cors());
